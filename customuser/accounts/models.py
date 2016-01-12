d982ecca72c7bb7a570279d51d7959fcaed57dca
Aniket Maithani <maithani.aniket@gmail.com>
2016-01-12 00:24:28 -0500

    feat(tests/): Added simple test cases for `custom user model` - updated requirements - deleted unrequired files from `accounts` app.

--- customuser/accounts/models.py
+++ customuser/accounts/models.py
@@ -20,20 +20,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     http://www.w3.org/International/questions/qa-personal-names
     """
     username = models.CharField(_('username'), max_length=30, unique=True,
-        help_text=_('Required. 30 characters or fewer. Letters, numbers and '
-                    '@/./+/-/_ characters'),
-        validators=[
-            validators.RegexValidator(re.compile('^[\w.@+-]+$'), _('Enter a valid username.'), 'invalid')
-        ])
+                                help_text=_('Required. 30 characters or fewer. Letters, numbers and '
+                                            '@/./+/-/_ characters'),
+                                validators=[
+        validators.RegexValidator(re.compile(
+            '^[\w.@+-]+$'), _('Enter a valid username.'), 'invalid')
+    ])
     full_name = models.CharField(_('full name'), max_length=254, blank=True)
     short_name = models.CharField(_('short name'), max_length=30, blank=True)
     email = models.EmailField(_('email address'), max_length=254, unique=True)
     is_staff = models.BooleanField(_('staff status'), default=False,
-        help_text=_('Designates whether the user can log into this admin '
-                    'site.'))
+                                   help_text=_('Designates whether the user can log into this admin '
+                                               'site.'))
     is_active = models.BooleanField(_('active'), default=True,
-        help_text=_('Designates whether this user should be treated as '
-                    'active. Unselect this instead of deleting accounts.'))
+                                    help_text=_('Designates whether this user should be treated as '
+                                                'active. Unselect this instead of deleting accounts.'))
     date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
 
     objects = UserManager()
--- customuser/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import include, patters, url
-from . import views
-
-
-urlpatters = patterns(
-    '',
-    url(r'^dashboard/$', views.dashboard, name='accounts_dashboard'),
-
-)
